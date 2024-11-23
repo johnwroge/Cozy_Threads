c927fcba82aaea3209950466363cd7fd377cfce2
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-11-23 17:14:03 -0500

    update

--- server/server.js
+++ server/server.js
@@ -31,7 +31,6 @@ const calculateOrderAmount = (items) => {
 };
 
 app.get("/api/config", (req, res) => {
-  console.log("api config");
 
   res.send({
     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
@@ -41,7 +40,6 @@ app.get("/api/config", (req, res) => {
 
 
 app.post("/api/create-payment-intent", async (req, res) => {
-  console.log("create payment intent");
 
   const { items } = req.body;
 
