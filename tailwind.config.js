1643c3f40b0a52d2d4c47fa1709ec18f0154a73b
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-12-12 09:55:42 -0500

    module error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,45 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//     darkMode: ["class"],
+//     content: [
+//       './index.html',
+//       './src/**/*.{js,ts,jsx,tsx}',
+//     ],
+//     theme: {
+//       container: {
+//         center: true,
+//         padding: "2rem",
+//         screens: {
+//           "2xl": "1400px",
+//         },
+//       },
+//       extend: {
+//         fontFamily: {
+//           'heading': ['"Playfair Display"', 'serif'],
+//           'body': ['Lora', 'serif'], 
+//         },
+//         keyframes: {
+//           "accordion-down": {
+//             from: { height: 0 },
+//             to: { height: "var(--radix-accordion-content-height)" },
+//           },
+//           "accordion-up": {
+//             from: { height: "var(--radix-accordion-content-height)" },
+//             to: { height: 0 },
+//           },
+//         },
+//         animation: {
+//           "accordion-down": "accordion-down 0.2s ease-out",
+//           "accordion-up": "accordion-up 0.2s ease-out",
+//         },
+//       },
+//     },
+//     plugins: [require("tailwindcss-animate")],
+//   }
+
 /** @type {import('tailwindcss').Config} */
+import animate from "tailwindcss-animate"
+
 export default {
     darkMode: ["class"],
     content: [
@@ -34,5 +75,5 @@ export default {
         },
       },
     },
-    plugins: [require("tailwindcss-animate")],
-  }
+    plugins: [animate],
+}
